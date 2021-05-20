549bff2295fe35912e4ab0f5ddd5549057a1569b
Nguyen Huu Phuoc <nguyenhuuphuocit97@gmail.com>
2021-05-20 13:52:50 +0700

    :tada: chore: init project #1

---
+++ src/main/java/com/phuocnguyen/app/sivaoswsc/SivaOsWscApplication.java
@@ -0,0 +1,13 @@
+package com.phuocnguyen.app.sivaoswsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivaOsWscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivaOsWscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phuocnguyen/app/sivaoswsc/SivaOsWscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuocnguyen.app.sivaoswsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivaOsWscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
